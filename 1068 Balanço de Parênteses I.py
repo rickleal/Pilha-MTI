d8daeecea9801f21554648c3f744b6f95095dc99
Manrick Leal <55140949+rickleal@users.noreply.github.com>
2021-06-02 10:19:51 -0300

    Create 1068 Balanço de Parênteses I.py

---
+++ 1068 Balanço de Parênteses I.py
@@ -0,0 +1,23 @@
+# Rick Leal
+
+if __name__ == '__main__':
+
+  while True:
+      try:
+          p = input()
+          d = 0
+          for i in range(len(p)):
+              if(p[i]=='('):
+                  d += 1
+              elif(p[i]==')'):
+                  d -= 1
+              if(d < 0):
+                  break
+          if(d != 0):
+              print('incorrect')
+          else:
+              print('correct')
+
+      except EOFError:
+          break
+        
