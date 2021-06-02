25e4ba993211f07f286ec129c3acd7c3c0e13325
Manrick Leal <55140949+rickleal@users.noreply.github.com>
2021-06-02 09:55:14 -0300

    Create 1069 - Diamantes e Areia.py

---
+++ 1069 - Diamantes e Areia.py
@@ -0,0 +1,18 @@
+#RIck Leal
+
+if __name__ == '__main__':
+
+qte = int(input())
+
+for i in range(qte):
+	linha = input()
+	total = 0
+	menor = 0
+	for j in range(len(linha)):
+		if(linha[j] == "<"):
+			menor += 1
+		if(linha[j] == ">" and menor > 0):
+			total += 1
+			menor -= 1
+
+	print(total)
